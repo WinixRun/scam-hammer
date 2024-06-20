695b212b184e973a00a676f135d60688cab345ee
Sergio <secwinix@gmail.com>
2024-06-20 23:51:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
